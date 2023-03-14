36a1562fa2d6031ec99fc2f3d7d8ac6ad22a7cb0
Aviral3004 <86399898+Aviral3004@users.noreply.github.com>
2023-03-14 09:55:53 +0530

    navbar small change

--- src/components/Buttons/NavButton.jsx
+++ src/components/Buttons/NavButton.jsx
@@ -3,7 +3,7 @@ import React, { Fragment } from "react";
 const NavButton = ({ text }) => {
   return (
     <Fragment>
-      <button className="hidden sm:flex font-spacegrotesk text-textColor font-medium text-md px-2 py-2 rounded-md">
+      <button className="hidden sm:flex font-spacegrotesk text-textColor font-medium text-md px-2 py-2 rounded-md ">
         {text}
       </button>
     </Fragment>
